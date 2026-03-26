dffc8ab33c0e6b0da2470df61176c0b439c488f6
Minnie-Mhie <aishaatinukeaisha@gmail.com>
2026-03-26 06:53:06 -0700

    fix navbar overflowing

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,11 @@ import { configureStore } from "@reduxjs/toolkit";
 import "./App.css"
 import appSlice from "./redux/appSlice.js"
 import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.bundle.min.js"
 import axiosInstance from "./api/axiosInstance.js";
+
+
 
 axios.get("https://separate-backend.vercel.app/api/v1/products").catch(() => {})
 
