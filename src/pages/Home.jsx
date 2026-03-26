e23056b8a40671af60d7a31a0859406fb652f654
Minnie-Mhie <aishaatinukeaisha@gmail.com>
2026-03-26 07:20:30 -0700

    fix navbar overflowing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -65,11 +65,11 @@ const Home = () => {
         </Link>
         <div className="home-nav-links">
           <a href="#about"   className="home-nav-link">About</a>
-          <a href="#vendors" className="home-nav-link">Vendor</a>
+          <a href="#vendors" className="home-nav-link">Vendors</a>
           <a href="#why"     className="home-nav-link">Why Us</a>
-          <a href="/contact" className="home-nav-link">Contact</a>
+          <a href="/contact" className="home-nav-link">Contact Us</a>
           <Link to="/login"    className="home-btn-outline">Login</Link>
-          <Link to="/register" className="home-btn-solid">Start</Link>
+          <Link to="/register" className="home-btn-solid">Get Started</Link>
         </div>
       </nav>
 
