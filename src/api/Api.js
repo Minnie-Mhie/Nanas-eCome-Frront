9ad868642933d723057117da2c67034f386602dc
Minnie-Mhie <aishaatinukeaisha@gmail.com>
2026-03-26 16:18:48 -0700

    fix toggle for side and nav bars

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api", 
+  baseURL: "https://separate-backend.vercel.app", 
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
